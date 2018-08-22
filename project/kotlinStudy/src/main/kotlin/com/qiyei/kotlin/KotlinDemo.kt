72e4ca7ed8fdcd401fe6079cabf0aa405576ea0d
qiyei2015 <1273482124@qq.com>
2018-08-22 21:07:19 +0800

    初始化工程

---
+++ project/kotlinStudy/src/main/kotlin/com/qiyei/kotlin/KotlinDemo.kt
@@ -0,0 +1,12 @@
+package com.qiyei.kotlin
+
+/**
+ * @author Created by qiyei2015 on 2018/8/22.
+ * @version: 1.0
+ * @email: 1273482124@qq.com
+ * @description:
+ */
+
+fun main(args:Array<String>){
+    println("hello world")
+}
